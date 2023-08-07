cb28d2595b4d5f314aac0f8f149322aa1764158a
tigawanna <denniskinuthiaw@gamil.com>
2023-08-07 07:19:51 +0300

    fix

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,13 +1,9 @@
-import { useRouter } from "@tanstack/router";
 import { SigninForm } from "./components/SigninForm";
 
 interface LoginUserProps {}
 
 export function LoginUser({}: LoginUserProps) {
-
-
-
-  return (
+ return (
     <div className="w-full h-full min-h-screen flex flex-col border items-center justify-center ">
     <SigninForm/>
 
